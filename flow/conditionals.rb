23adf254a889953d4a7c820b5522214bb5c136bd
Oluwafemi Sule <oluwafemisule@outlook.com>
2016-03-22 07:25:46 +0100

    learn to use conditionals

---
+++ flow/conditionals.rb
@@ -0,0 +1,14 @@
+# nil and false are falsy,
+# everything else is truthy.
+
+if some_variable.is_a?(String)
+    # do something
+else
+    # do something else
+end
+
+# unless := opposite of if
+
+unless (1+1).equals?(2)
+    puts "summed correctly" # this doesn't get printed.
+end
