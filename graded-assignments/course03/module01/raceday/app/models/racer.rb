908ce70652ed45037d3b9eec73d84006be4c19a7
Oluwafemi Sule <oluwafemisule@outlook.com>
2016-12-04 21:45:42 +0100

    integrate MongoDB Ruby Driver and Rails

---
+++ graded-assignments/course03/module01/raceday/app/models/racer.rb
@@ -0,0 +1,96 @@
+class Racer
+  include ActiveModel::Model
+  attr_accessor :id, :number, :first_name, :last_name, :gender, :group, :secs
+
+  def persisted?
+    !@id.nil?
+  end
+
+  def created_at
+    nil
+  end
+
+  def updated_at
+    nil
+  end
+
+  def initialize(params={})
+    @id=params[:_id].nil? ? params[:id] : params[:_id].to_s
+    @number=params[:number].to_i
+    @first_name=params[:first_name]
+    @last_name=params[:last_name]
+    @gender=params[:gender]
+    @group=params[:group]
+    @secs=params[:secs].to_i
+  end
+
+  def self.mongo_client
+    Mongoid::Clients.default
+  end
+
+  def self.collection
+    self.mongo_client['racers']
+  end
+
+  def self.all(prototype={}, sort={:number=>1}, skip=0, limit=nil)
+    prototype=prototype.symbolize_keys.slice(:number, :first_name, :last_name, :gender, :group, :secs) if !prototype.nil?
+    result = collection.find(prototype)
+            .sort(sort)
+            .skip(skip)
+    result  = result.limit(limit) if !limit.nil?
+    return result
+  end
+
+  def self.paginate(params)
+    page=(params[:page] || 1).to_i
+    limit=(params[:per_page] || 30).to_i
+    skip=(page-1)*limit
+
+    racers=[]
+    all(params, {:number=>1}, skip, limit).each do |racer|
+      racers << Racer.new(racer)
+    end
+
+    total=all(params, {:number=>1}, 0, 1).count
+
+    WillPaginate::Collection.create(page, limit, total) do |pager|
+      pager.replace(racers)
+    end
+  end
+
+  def self.find(id)
+    id = BSON::ObjectId.from_string(id) if id.instance_of? String
+    result = collection.find(:_id=>id).first
+    return result.nil? ? nil : Racer.new(result)
+  end
+
+  def save
+    result=self.class.collection
+            .insert_one(
+              _id:@id, number:@number,
+              first_name:@first_name, last_name:@last_name,
+              gender:@gender, group:@group,
+              secs:@secs)
+    @id = result.inserted_id.to_s
+  end
+
+  def update(params)
+    @number=params[:number].to_i
+    @first_name=params[:first_name]
+    @last_name=params[:last_name]
+    @group=params[:group]
+    @gender=params[:gender]
+    @secs=params[:secs].to_i
+    params.slice!(:number, :first_name, :last_name, :gender, :group, :secs) if !params.nil?
+
+    self.class.collection
+              .find(_id:BSON::ObjectId.from_string(@id))
+              .update_one(params)
+  end
+
+  def destroy
+    self.class.collection
+              .find(number:@number)
+              .delete_one
+  end
+end
