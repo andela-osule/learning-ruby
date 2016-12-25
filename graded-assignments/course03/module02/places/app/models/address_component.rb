81fa7ee08a57ae00dc2b7a73211b492ce8b7932e
Oluwafemi Sule <oluwafemisule@outlook.com>
2016-12-25 22:46:23 +0100

    standard queries (wip)

--- graded-assignments/course03/module02/places/app/models/address_component.rb
+++ graded-assignments/course03/module02/places/app/models/address_component.rb
@@ -4,6 +4,7 @@ class AddressComponent
     attr_reader :types
 
     def initialize(address)
+        address = address.symbolize_keys
         @long_name = address[:long_name] if address.key? :long_name
         @short_name = address[:short_name] if address.key? :short_name
         @types = address[:types] if address.key? :types
--- graded-assignments/course03/module02/places/app/models/place.rb
+++ graded-assignments/course03/module02/places/app/models/place.rb
@@ -5,11 +5,13 @@ class Place
     attr_accessor :address_components
 
     def initialize(place)
-        @id = place[:_id].to_s
-        @location = Point.new(place[:geometry][:location])
+        place = place.symbolize_keys
 
-        @address_components = []
-        place[:address_components].reduce([]){|memo,chunk| @address_components<< AddressComponent.new(chunk)}
+        @id = place[:_id].to_s
+        geometry = place[:geometry].symbolize_keys
+        @location = Point.new(geometry[:geolocation])
+
+        @address_components = place[:address_components].reduce([]){|memo,chunk| memo<< AddressComponent.new(chunk)}
         
         @formatted_address = place[:formatted_address]     
     end
@@ -24,5 +26,32 @@ class Place
 
     def self.load_all(f)
         collection.insert_many(JSON.parse(f.read))    
+    end
+
+    def self.find_by_short_name(short_name)
+        collection.find({"address_components" => {:$elemMatch=>{"short_name"=>short_name}}})
+    end
+
+    def self.to_places(view)
+        view.reduce([]) {|memo, place| memo<<Place.new(place)}
+    end
+
+    def self.find(id)
+        place = collection.find({:_id=>BSON::ObjectId.from_string(id)}).first
+        unless place.nil?
+            Place.new(place)
+        end
+    end
+
+    def self.all(offset=0, limit=0)
+        to_places(
+            collection.find.skip(offset).limit(limit)
+        )
+    end
+
+    def destroy
+        self.class.collection.find_one_and_delete({
+            :_id=>BSON::ObjectId.from_string(@id)
+        })
     end
 end
