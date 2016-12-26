1847a70bd1f59f4d53c41c5401729942e4870a63
Oluwafemi Sule <oluwafemisule@outlook.com>
2016-12-26 23:00:47 +0100

    implement many to one relationship

--- graded-assignments/course03/module02/places/app/models/photo.rb
+++ graded-assignments/course03/module02/places/app/models/photo.rb
@@ -1,6 +1,7 @@
 class Photo
   attr_accessor :id
   attr_accessor :location
+  attr_accessor :place
   attr_writer :contents
 
   def self.mongo_client
@@ -10,6 +11,7 @@ class Photo
   def initialize(params={})
     @id = params[:_id].to_s unless params.nil? || params[:_id].nil?
     @location = Point.new(params[:metadata][:location]) unless params.nil? || params[:metadata].nil?
+    @place = params[:metadata][:place] unless params.nil? || params[:metadata].nil?
   end
 
   def persisted?
@@ -20,18 +22,21 @@ class Photo
     unless persisted?
       gps = EXIFR::JPEG.new(@contents).gps
       @location=Point.new({:lng=>gps.longitude, :lat=>gps.latitude})
-
       description = {
         :content_type=>"image/jpeg",
         :metadata=>{
-          :location=>@location.to_hash
+          :location=>@location.to_hash,
+          :place=>@place
         }
       }
       grid_file = Mongo::Grid::File.new(@contents.read, description)
-      unless @contents.nil?
-        id = self.class.mongo_client.database.fs.insert_one(grid_file)
-        @id = id.to_s
-      end
+      @id = self.class.mongo_client.database.fs.insert_one(grid_file).to_s unless @contents.nil?
+    else
+      doc = self.class.mongo_client.database.fs.find(:_id=>BSON::ObjectId.from_string(@id)).first
+      doc[:metadata] = {:location=>@location.to_hash, :place=>@place}
+      self.class.mongo_client.database.fs.find(
+        '_id': BSON::ObjectId.from_string(@id)
+      ).update_one(doc)
     end
   end
 
@@ -52,5 +57,22 @@ class Photo
 
   def destroy
     self.class.mongo_client.database.fs.find(:_id=>BSON::ObjectId.from_string(@id)).delete_one
+  end
+
+  def find_nearest_place_id(max_dist)
+    place_id = Place.near(@location, max_dist).limit(1).projection(:_id=>1).first
+    place_id[:_id] || 0
+  end
+
+  def place
+    unless @place.nil?
+      Place.find(@place.to_s)
+    end
+  end
+
+  def place=(_place)
+    @place = BSON::ObjectId.from_string(_place.id) if _place.is_a? Place
+    @place = BSON::ObjectId.from_string(_place) if _place.is_a? String
+    @place = _place if BSON::ObjectId
   end
 end
