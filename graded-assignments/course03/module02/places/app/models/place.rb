6b29aed93b0bf22814c7093178e8ea2b2678c859
Oluwafemi Sule <oluwafemisule@outlook.com>
2016-12-26 12:18:28 +0100

    more aggregation * get address components, country names, ids by country code * create and remove indexes * find places near geolocation

--- graded-assignments/course03/module02/places/app/models/place.rb
+++ graded-assignments/course03/module02/places/app/models/place.rb
@@ -54,4 +54,51 @@ class Place
             :_id=>BSON::ObjectId.from_string(@id)
         })
     end
+
+    def self.get_address_components(sort=nil, offset=nil, limit=nil)
+        aggregation = collection.find.aggregate([
+            {:$unwind=>"$address_components"},
+            {:$project=>{:_id=>1, :address_components=>1, :formatted_address=>1, :geometry=>{:geolocation=>1}}}
+        ])
+        aggregation.pipeline << {:$sort=>sort} unless sort.nil?
+        aggregation.pipeline << {:$skip=>offset} unless offset.nil?
+        aggregation.pipeline << {:$limit=>limit} unless offset.nil? 
+        return aggregation
+    end
+
+    def self.get_country_names
+        collection.find.aggregate([
+            {:$unwind=>"$address_components"},
+            {:$project=>{:_id=>1, :address_components=>{:types=>1, :long_name=>1}}},
+            {:$match=>{"address_components.types"=>{:$eq=>"country"}}},
+            {:$group=>{:_id=>"$address_components.long_name"}}
+        ]).to_a.map {|h| h[:_id]}
+    end
+
+    def self.find_ids_by_country_code(country_code)
+        collection.find.aggregate([
+            {:$unwind=>"$address_components"},
+            {:$match=>{"address_components.short_name"=>{:$eq=>country_code}}},
+            {:$project=>{:_id=>1}}
+        ]).map {|doc| doc[:_id].to_s}
+    end
+
+    def self.create_indexes
+        collection.indexes.create_one({"geometry.geolocation"=>Mongo::Index::GEO2DSPHERE})
+    end
+
+    def self.remove_indexes
+        collection.indexes.drop_one("geometry.geolocation_2dsphere")
+    end
+
+    def self.near(point, max_meters=nil)
+        near = {
+            :$near=>{
+                :$geometry=>point.to_hash,
+            }
+        }
+        near[:$near][:$maxDistance] = max_meters unless max_meters.nil?
+        
+        collection.find({"geometry.geolocation"=>near})
+    end
 end
