0c1ac26fa06e849f52611c57744163222fcc0ab6
Oluwafemi Sule <oluwafemisule@outlook.com>
2016-12-28 21:45:10 +0100

    map the class properties to documentation properties found within our documents

--- graded-assignments/course03/module03/triracers/app/models/racer.rb
+++ graded-assignments/course03/module03/triracers/app/models/racer.rb
@@ -1,3 +1,8 @@
 class Racer
   include Mongoid::Document
+  field :first_name, type: String, as: :fn
+  field :last_name, type: String, as: :ln
+  field :date_of_birth, type: Date, as: :dob
+  field :gender, type: String
+  store_in collection: 'racer1'
 end
