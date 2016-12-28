e31e029c439346370927491f766b795535a58737
Oluwafemi Sule <oluwafemisule@outlook.com>
2016-12-28 21:52:53 +0100

    add timestamps

--- graded-assignments/course03/module03/triracers/app/models/racer.rb
+++ graded-assignments/course03/module03/triracers/app/models/racer.rb
@@ -1,5 +1,6 @@
 class Racer
   include Mongoid::Document
+  include Mongoid::Timestamps
   field :first_name, type: String, as: :fn
   field :last_name, type: String, as: :ln
   field :date_of_birth, type: Date, as: :dob
