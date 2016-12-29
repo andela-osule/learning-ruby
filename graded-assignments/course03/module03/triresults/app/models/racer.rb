8f8c81eda7ef0079c73eba4ea588f11bf8f1220a
Oluwafemi Sule <oluwafemisule@outlook.com>
2016-12-29 23:49:50 +0100

    implement racers collection

---
+++ graded-assignments/course03/module03/triresults/app/models/racer.rb
@@ -0,0 +1,10 @@
+class Racer
+  include Mongoid::Document
+
+  embeds_one :info, class_name: "RacerInfo", as: :addressable, autobuild: true
+
+
+  before_create do |racer|
+    racer.info.id = racer.id
+  end
+end
