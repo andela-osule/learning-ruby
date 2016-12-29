a0e9432e54ffea44924fc253ec7bda8793f4ed56
Oluwafemi Sule <oluwafemisule@outlook.com>
2016-12-29 09:34:53 +0100

    implement 1-1 relationship between documents

---
+++ graded-assignments/course03/module03/contests/app/models/racer.rb
@@ -0,0 +1,8 @@
+class Racer
+  include Mongoid::Document
+  field :fn, as: :first_name, type: String
+  field :ln, as: :last_name, type: String
+  field :dob, as: :date_of_birth, type: Date
+
+  embeds_one :primary_address, as: :addressable, class_name: 'Address'
+end
