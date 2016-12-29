51d446b15d7b1c74be99f2c114b1be2421c0e42c
Oluwafemi Sule <oluwafemisule@outlook.com>
2016-12-29 08:03:44 +0100

    remove unrequired specifics

--- graded-assignments/course03/module03/triracers/config/application.rb
+++ graded-assignments/course03/module03/triracers/config/application.rb
@@ -20,9 +20,6 @@ module Triracers
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     Mongoid.load!("./config/mongoid.yml") 
-    config.generators {|g| g.orm :mongoid} 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
-    config.web_console.development_only = false
   end
 end
